95bf003035a73405e81bf26a7a29cddb1c1440a6
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-04-12 15:36:57 -0700

    Got the Js fully working

--- js/index.js
+++ js/index.js
@@ -4,10 +4,14 @@ let button = document.querySelector('#button')
 
 button.addEventListener("click", addNums)
 
+
+
 function addNums() {
-  let mineralsPrice = document.getElementById('amount').value
-  console.log(mineralsPrice)
-  let PricePaid = document.getElementById('paid').value
-  console.log(PricePaid)
-  console.log("Hello World")
+  let mineralsPrice = Number(document.getElementById('amount').value)
+  let pricePaid = Number(document.getElementById('paid').value) 
+  let premiumPaid = pricePaid - mineralsPrice
+  let percentage = (( (pricePaid - mineralsPrice) / ( (mineralsPrice + pricePaid ) / 2 ) ) * 100).toFixed(2)
+
+  document.getElementById('premium').innerHTML = `$${premiumPaid}`
+  document.getElementById('percent').innerHTML = `${percentage}%`
 }
